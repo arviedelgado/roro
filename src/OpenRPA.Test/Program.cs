ab6922aec68538aab7a8962f5dd8898086546169
Arvie <arviedelgado@outlook.com>
2017-09-24 05:29:42 +0800

    Added Inspect() and Query() methods to Desktop class

--- src/OpenRPA.Test/Program.cs
+++ src/OpenRPA.Test/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using OpenRPA.Core;
+using System.Linq;
+using System.Diagnostics;
 
 namespace OpenRPA.Test
 {
@@ -8,22 +10,32 @@ namespace OpenRPA.Test
         static void Main()
         {
             var desktop = new Desktop();
-            //var context = desktop.LaunchChrome("http://example.com");
-            //context.ExecuteScript(@"
-            //    var iframe = document.body.appendChild(document.createElement('iframe'));
-            //    iframe.src = 'http://example.com';
-            //    iframe.style.cssText = 'position: fixed; top: 0; left: 0;';
-            //");
+            var context = desktop.LaunchChrome("http://example.com");
+            context.ExecuteScript(@"
+                var iframe = document.body.appendChild(document.createElement('iframe'));
+                iframe.src = 'http://example.com';
+                iframe.style.cssText = 'position: fixed; top: 0; left: 0;';
+            ");
 
-            //context.ExecuteScript(@"
-            //    var iframe = document.body.appendChild(document.createElement('iframe'));
-            //    iframe.src = 'http://example.com';
-            //    iframe.style.cssText = 'position: fixed; bottom: 0; right: 0;';
-            //");
+            context.ExecuteScript(@"
+                var iframe = document.body.appendChild(document.createElement('iframe'));
+                iframe.src = 'http://example.com';
+                iframe.style.cssText = 'position: fixed; bottom: 0; right: 0;';
+            ");
 
-            //            desktop.LaunchEdge("http://rpaaas.com");
-            //            desktop.LaunchInternetExplorer("http://rpaaas.com");
-            Console.ReadLine();
+
+            while (true)
+            {
+                var query = desktop.Inspect();
+                var sw = Stopwatch.StartNew();
+                var elements = desktop.Query(query);
+                Console.WriteLine("Result: {0} elements found in {1} seconds", elements.Count(), sw.ElapsedMilliseconds / 1000.0);
+                Console.WriteLine();
+                foreach (var el in elements)
+                {
+                    Console.WriteLine("{0}\t{1}", el.Bounds, el.Path);
+                }
+            }
         }
     }
 }

68a15c755b47c5706b3fc34a015d590b35b6eac3
Arvie <arviedelgado@outlook.com>
2017-10-26 09:53:55 +0800

    October Updates - Add Basic Excel Activities

---
+++ src/Roro.Core.Activities.Excel/CreateWorkbook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Roro.Core.Activities.Excel
+{
+    public class CreateWorkbook : Activity
+    {
+        public OutArgument<string> Path { get; set; }
+
+        public override void Execute(Context context)
+        {
+            // inputs
+            //
+
+            var bot = ExcelBot.Shared;
+            var xlApp = bot.Get();
+            var xlWbs = xlApp.Workbooks;
+            var xlWb = xlWbs.Add();
+            var path = xlWb.FullName;
+            bot.Release(xlWb, xlWbs);
+
+            // outputs
+            context.Set(this.Path, path);
+        }
+    }
+}

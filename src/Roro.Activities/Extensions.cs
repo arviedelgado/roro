d25acf89e9c0defe8d13d6e24aec4e316ade9a8f
Arvie <arviedelgado@outlook.com>
2017-12-11 01:59:13 +0800

    Roro Studio KL - Add Extensions class. Say no more.

---
+++ src/Roro.Activities/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Roro.Activities
+{
+    public static class Extensions
+    {
+        public static string Humanize(this string str)
+        {
+            return Regex.Replace(str.Split('.').Last(), "([a-z](?=[A-Z0-9])|[A-Z](?=[A-Z][a-z]))", "$1 ");
+        }
+    }
+}

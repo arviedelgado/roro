773cc3fdec451c995ce171a91145a3536787e4ae
Arvie <arviedelgado@outlook.com>
2017-12-11 01:55:57 +0800

    Roro Studio KL - Add InpOutArgument for PreparationNodeActivity

--- src/Roro.Activities/Argument.cs
+++ src/Roro.Activities/Argument.cs
@@ -1,14 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
 using System.Runtime.Serialization;
-using System.Windows.Forms;
 
 namespace Roro.Activities
 {
     [DataContract]
-    public abstract class Input
+    public abstract class Argument
+    {
+
+    }
+
+    [DataContract]
+    public abstract class InArgument : Argument
     {
         [DataMember]
         public string Name { get; set; }
@@ -21,11 +23,11 @@ namespace Roro.Activities
     }
 
     [DataContract]
-    public sealed class Input<T> : Input where T : DataType, new()
+    public sealed class InArgument<T> : InArgument where T : DataType, new()
     {
-        public Input() : this(string.Empty) { }
+        public InArgument() : this(string.Empty) { }
 
-        public Input(string name)
+        public InArgument(string name)
         {
             this.Name = name;
             this.DataTypeId = new T().Id;
@@ -34,7 +36,7 @@ namespace Roro.Activities
     }
 
     [DataContract]
-    public abstract class Output
+    public abstract class OutArgument : Argument
     {
         [DataMember]
         public string Name { get; set; }
@@ -47,11 +49,11 @@ namespace Roro.Activities
     }
 
     [DataContract]
-    public sealed class Output<T> : Output where T : DataType, new()
+    public sealed class OutArgument<T> : OutArgument where T : DataType, new()
     {
-        public Output() : this(string.Empty) { }
+        public OutArgument() : this(string.Empty) { }
 
-        public Output(string name)
+        public OutArgument(string name)
         {
             this.Name = name;
             this.DataTypeId = new T().Id;
@@ -59,217 +61,27 @@ namespace Roro.Activities
         }
     }
 
-    public class ActivityGrid : DataGridView
+    [DataContract]
+    public abstract class InOutArgument : Argument
     {
-        private void InitializeComponent()
+        [DataMember]
+        public Guid VariableId { get; set; }
+
+        [DataMember]
+        public string DataTypeId { get; set; }
+
+        [DataMember]
+        public string Expression { get; set; }
+    }
+
+    [DataContract]
+    public sealed class InOutArgument<T> : InOutArgument where T : DataType, new()
+    {
+        public InOutArgument()
         {
-            // grid
-            this.AllowUserToAddRows = false;
-            this.AllowUserToDeleteRows = false;
-            this.AutoGenerateColumns = false;
-            this.SelectionMode = DataGridViewSelectionMode.CellSelect;
-            this.EnableHeadersVisualStyles = false;
-            this.ShowEditingIcon = false;
-            this.Dock = DockStyle.Fill;
-            this.BorderStyle = BorderStyle.None;
-            this.BackgroundColor = Color.White;
-            this.MultiSelect = false;
-            this.DefaultCellStyle.Padding = new Padding(1);
-
-            // columns
-            this.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            this.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-
-            // rows
-            this.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
-            this.RowHeadersVisible = false;
-            this.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
-
-            // cells
-            this.EditMode = DataGridViewEditMode.EditOnKeystrokeOrF2;
-        }
-
-        public ActivityGrid(List<Input> inputs)
-        {
-            this.InitializeComponent();
-
-            this.Columns.Add(new DataGridViewTextBoxColumn
-            {
-                Name = "Name",
-                DataPropertyName = "Name",
-                FillWeight = 35
-            });
-
-            this.Columns.Add(new DataTypeColumn
-            {
-                Name = "Type",
-                DataPropertyName = "DataTypeId",
-                FillWeight = 15
-            });
-
-            this.Columns.Add(new DataGridViewTextBoxColumn
-            {
-                Name = "Value",
-                DataPropertyName = "Expression",
-                FillWeight = 50
-            });
-
-            this.DataSource = new BindingList<Input>(inputs)
-            {
-                AllowNew = true,
-                AllowEdit = true,
-                AllowRemove = true
-            };
-        }
-
-        public ActivityGrid(List<Output> outputs)
-        {
-            this.InitializeComponent();
-
-            var variables = new List<Variable>()
-            {
-                new Variable<Number>(),
-                new Variable<Text>("Var1"),
-                new Variable<Text>("Var2"),
-                new Variable<Text>("Var3")
-            };
-
-            this.Columns.Add(new DataGridViewTextBoxColumn
-            {
-                Name = "Name",
-                DataPropertyName = "Name",
-                FillWeight = 35
-            });
-
-            this.Columns.Add(new DataTypeColumn
-            {
-                Name = "Type",
-                DataPropertyName = "DataTypeId",
-                FillWeight = 15
-            });
-
-            this.Columns.Add(new VariableColumn
-            {
-                Name = "Variable",
-                DataPropertyName = "VariableId",
-                FillWeight = 50,
-                DataSource = variables
-            });
-
-            this.DataSource = new BindingList<Output>(outputs)
-            {
-                AllowNew = true,
-                AllowEdit = true,
-                AllowRemove = true
-            };
-        }
-
-        protected override void OnDataError(bool displayErrorDialogIfNoHandler, DataGridViewDataErrorEventArgs e)
-        {
-            e.ThrowException = true;
-            if (this[e.ColumnIndex, e.RowIndex] is VariableCell variableCell)
-            {
-                variableCell.OnDataError(this, e);
-            }
-            if (this[e.ColumnIndex, e.RowIndex] is DataTypeCell dataTypeCell)
-            {
-                dataTypeCell.OnDataError(this, e);
-            }
-            if (e.ThrowException)
-            {
-                base.OnDataError(displayErrorDialogIfNoHandler, e);
-            }
-        }
-
-        protected override void OnCellParsing(DataGridViewCellParsingEventArgs e)
-        {
-            if (this.CurrentCell.OwningColumn is DataGridViewComboBoxColumn)
-            {
-                var edit = this.EditingControl as DataGridViewComboBoxEditingControl;
-                e.Value = edit.SelectedItem;
-                e.ParsingApplied = true;
-            }
-            base.OnCellParsing(e);
-        }
-
-        internal void AddRow(object sender, EventArgs args)
-        {
-            var grid = this;
-            var list = (grid.DataSource as IBindingList);
-            if (grid.CurrentCell is DataGridViewCell cell && cell.Selected)
-            {
-                grid.ClearSelection();
-                if (list is BindingList<Input>)
-                {
-                    list.Insert(cell.RowIndex + 1, new Input<Text>());
-                }
-                else
-                {
-                    list.Insert(cell.RowIndex + 1, new Output<Text>());
-                }
-                grid.CurrentCell = grid[0, cell.RowIndex + 1];
-                grid.Focus();
-            }
-            else
-            {
-                grid.ClearSelection();
-                if (list is BindingList<Input>)
-                {
-                    list.Add(new Input<Text>());
-                }
-                else
-                {
-                    list.Add(new Output<Text>());
-                }
-                grid.CurrentCell = grid[0, grid.Rows.Count - 1];
-                grid.Focus();
-            }
-        }
-
-        internal void RemoveRow(object sender, EventArgs args)
-        {
-            var grid = this;
-            var list = (grid.DataSource as IBindingList);
-            if (grid.CurrentCell is DataGridViewCell cell && cell.Selected)
-            {
-                grid.ClearSelection();
-                list.RemoveAt(cell.RowIndex);
-                grid.Focus();
-            }
-        }
-
-        internal void MoveUpRow(object sender, EventArgs args)
-        {
-            var grid = this;
-            var list = (grid.DataSource as IBindingList);
-            if (grid.CurrentCell is DataGridViewCell cell && cell.Selected && cell.RowIndex > 0)
-            {
-                grid.ClearSelection();
-                var rowIndex = cell.RowIndex;
-                var columnIndex = cell.ColumnIndex;
-                var item = list[cell.RowIndex];
-                list.RemoveAt(rowIndex);
-                list.Insert(rowIndex - 1, item);
-                grid.CurrentCell = grid[columnIndex, rowIndex - 1];
-                grid.Focus();
-            }
-        }
-
-        internal void MoveDownRow(object sender, EventArgs args)
-        {
-            var grid = this;
-            var list = (grid.DataSource as IBindingList);
-            if (grid.CurrentCell is DataGridViewCell cell && cell.Selected && cell.RowIndex < grid.Rows.Count - 1)
-            {
-                grid.ClearSelection();
-                var rowIndex = cell.RowIndex;
-                var columnIndex = cell.ColumnIndex;
-                var item = list[cell.RowIndex];
-                list.RemoveAt(rowIndex);
-                list.Insert(rowIndex + 1, item);
-                grid.CurrentCell = grid[columnIndex, rowIndex + 1];
-                grid.Focus();
-            }
+            this.VariableId = Guid.Empty;
+            this.DataTypeId = new T().Id;
+            this.Expression = string.Empty;
         }
     }
 }
--- src/Roro.Activities/PreparationNodeActivity.cs
+++ src/Roro.Activities/PreparationNodeActivity.cs
@@ -1,17 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.Serialization;
 
 namespace Roro.Activities
 {
+    [DataContract]
     public sealed class PreparationNodeActivity : Activity
     {
-        public override void Execute(ActivityContext context)
-        {
-            throw new NotImplementedException();
-        }
+        public override bool AllowUserToEditArgumentRowList => true;
 
-        public PreparationNodeActivity()
-        {
+        public override bool AllowUserToEditArgumentColumn1 => true;
 
+        public override bool AllowUserToEditArgumentColumn2 => true;
+
+        public override bool AllowUserToEditArgumentColumn3 => true;
+
+        [DataMember]
+        private List<InOutArgument> InOutArguments { get; set; }
+
+        protected override List<InOutArgument> GetInOutArguments() => this.InOutArguments;
+        
+        public PreparationNodeActivity() => this.InOutArguments = new List<InOutArgument>();
+
+        public void Execute(ActivityContext context)
+        {
+            foreach (var inputOutput in this.InOutArguments)
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }

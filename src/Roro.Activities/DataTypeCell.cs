6f59c3c220859cdee16d99b8f93e6960edfad705
Arvie Delgado <11804514+arviedelgado@users.noreply.github.com>
2017-12-12 14:47:19 +0800

    Roro Studio KL - Bind to DataGridView.DataSource only when the DataGridView is on a visible form, to avoid DataGridView internal rendering / initialization exceptions.

--- src/Roro.Activities/DataTypeCell.cs
+++ src/Roro.Activities/DataTypeCell.cs
@@ -14,32 +14,5 @@ namespace Roro.Activities
             };
             e.ThrowException = false;
         }
-
-        protected override void OnDataGridViewChanged()
-        {
-            // Weird.
-
-            // A NullReferenceException occur
-            // when calling ArgumentForm.Create<T>(Activity activity, ...) method
-            // where activity is an instance of ProcessNodeActivity or DecisionNodeActivity.
-            // Other activities (Start-, End-, Preparation-, ...) does not throw this error.
-
-            // To handle, this for now, override OnDataGridViewChanged (this) method.
-
-            // System.NullReferenceException: Object reference not set to an instance of an object.
-            // at System.Windows.Forms.DataGridViewComboBoxCell.InitializeDisplayMemberPropertyDescriptor(String displayMember)
-            // at System.Windows.Forms.DataGridViewComboBoxCell.OnDataGridViewChanged()
-            // at System.Windows.Forms.DataGridViewRowCollection.get_Item(Int32 index)
-            // at System.Windows.Forms.DataGridView.AutoResizeRowInternal(Int32 rowIndex, DataGridViewAutoSizeRowMode autoSizeRowMode, Boolean fixedWidth, Boolean internalAutosizing)
-            // at System.Windows.Forms.DataGridView.AdjustShrinkingRows(DataGridViewAutoSizeRowsMode autoSizeRowsMode, Boolean fixedWidth, Boolean internalAutosizing)
-            // at System.Windows.Forms.DataGridView.PerformLayoutPrivate(Boolean useRowShortcut, Boolean computeVisibleRows, Boolean invalidInAdjustFillingColumns, Boolean repositionEditingControl)
-            // at System.Windows.Forms.DataGridView.OnHandleCreated(EventArgs e)
-            // at System.Windows.Forms.Control.WmCreate(Message & m)
-            // at System.Windows.Forms.Control.WndProc(Message & m)
-            // at System.Windows.Forms.DataGridView.WndProc(Message & m)
-            // at System.Windows.Forms.Control.ControlNativeWindow.OnMessage(Message & m)
-            // at System.Windows.Forms.Control.ControlNativeWindow.WndProc(Message & m)
-            // at System.Windows.Forms.NativeWindow.Callback(IntPtr hWnd, Int32 msg, IntPtr wparam, IntPtr lparam)
-        }
     }
 }

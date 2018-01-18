fc494c51f206debd552ff58cc9651b2e9c474e67
Arvie Delgado <11804514+arviedelgado@users.noreply.github.com>
2018-01-18 23:03:51 +0800

    Roro Studio KL - Updated UI for Activity Properties.

--- src/Roro.Activities/EndNodeActivity.cs
+++ src/Roro.Activities/EndNodeActivity.cs
@@ -1,31 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.Serialization;
+﻿using System.Runtime.Serialization;
 
 namespace Roro.Activities
 {
     [DataContract]
     public sealed class EndNodeActivity : Activity
     {
-        public override bool AllowUserToEditArgumentRowList => true;
+        //public override bool AllowUserToEditArgumentRowList => true;
 
-        public override bool AllowUserToEditArgumentColumn1 => true;
+        //public override bool AllowUserToEditArgumentColumn1 => true;
 
-        public override bool AllowUserToEditArgumentColumn2 => true;
+        //public override bool AllowUserToEditArgumentColumn2 => true;
 
-        public override bool AllowUserToEditArgumentColumn3 => true;
+        //public override bool AllowUserToEditArgumentColumn3 => true;
 
-        [DataMember]
-        private List<InArgument> InArguments { get; set; }
+        //[DataMember]
+        //private List<InArgument> InArguments { get; set; }
 
-        protected override List<InArgument> GetInArguments() => this.InArguments;
+        //protected override List<InArgument> GetInArguments() => this.InArguments;
 
-        public EndNodeActivity() => this.InArguments = new List<InArgument>();
+        //public EndNodeActivity() => this.InArguments = new List<InArgument>();
 
         public void Execute(ActivityContext context)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
     }
 }
--- src/Roro.Activities/StartNodeActivity.cs
+++ src/Roro.Activities/StartNodeActivity.cs
@@ -1,27 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.Serialization;
+﻿using System.Runtime.Serialization;
 
 namespace Roro.Activities
 {
     [DataContract]
     public sealed class StartNodeActivity : Activity
     {
-        public override bool AllowUserToEditArgumentRowList => true;
+        //public override bool AllowUserToEditArgumentRowList => true;
 
-        public override bool AllowUserToEditArgumentColumn1 => true;
+        //public override bool AllowUserToEditArgumentColumn1 => true;
 
-        public override bool AllowUserToEditArgumentColumn2 => true;
+        //public override bool AllowUserToEditArgumentColumn2 => true;
 
-        public override bool AllowUserToEditArgumentColumn3 => true;
+        //public override bool AllowUserToEditArgumentColumn3 => true;
 
-        [DataMember]
-        private List<OutArgument> OutArguments { get; set; }
+        //[DataMember]
+        //private List<OutArgument> OutArguments { get; set; }
 
-        protected override List<OutArgument> GetOutArguments() => this.OutArguments;
+        //protected override List<OutArgument> GetOutArguments() => this.OutArguments;
 
-        public StartNodeActivity() => this.OutArguments = new List<OutArgument>();
+        //public StartNodeActivity() => this.OutArguments = new List<OutArgument>();
 
         public void Execute(ActivityContext context)
         {

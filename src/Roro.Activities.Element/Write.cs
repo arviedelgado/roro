3d99a47140e8be455bad2f8f9e793fc7d9a63532
Arvie Delgado <11804514+arviedelgado@users.noreply.github.com>
2018-02-08 09:33:41 +0800

    Roro Studio - Added Element Exists and Write activities. It is now easier to add activities since the robot framework is already pretty.

--- src/Roro.Activities.Element/Write.cs
+++ src/Roro.Activities.Element/Write.cs
@@ -3,15 +3,18 @@ using System.Linq;
 
 namespace Roro.Activities.Element
 {
-    public sealed class Click : ProcessNodeActivity
+    public sealed class Write : ProcessNodeActivity
     {
         public Input<ElementQuery> Element { get; set; }
+
+        public Input<Text> Text { get; set; }
 
         public override void Execute(ActivityContext context)
         {
             var query = ElementQuery.Get(this.Element);
+
             if (query == null)
-                return;
+                throw new Exception("Input 'Element' is required.");
 
             var elements = WinContext.Shared.GetElementsFromQuery(query);
             if (elements.Count() == 0)
@@ -19,13 +22,13 @@ namespace Roro.Activities.Element
             if (elements.Count() > 1)
                 throw new Exception("Too many elements found.");
 
+            var text = context.Get(this.Text);
+
             using (var input = new InputDriver())
             {
                 var e = elements.First() as WinElement;
                 e.Focus();
-                var p = e.Bounds.Center();
-                input.MouseMove(p.X, p.Y);
-                input.Click(MouseButton.Left);
+                input.Write(text);
             }
         }
     }

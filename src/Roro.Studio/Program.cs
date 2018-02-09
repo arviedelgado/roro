0669c5dbf20fc798a32885160ad82a92f505643b
Arvie Delgado <11804514+arviedelgado@users.noreply.github.com>
2018-02-09 23:28:49 +0800

    Roro Studio - SetProcessDPIAware!

--- src/Roro.Studio/Program.cs
+++ src/Roro.Studio/Program.cs
@@ -1,18 +1,23 @@
 ﻿
 using Roro.Activities;
 using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace Roro.Studio
 {
     class Program
     {
+        [DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+
         [STAThread]
         static void Main(string[] args)
         {
+            SetProcessDPIAware();
+            Application.EnableVisualStyles();
             using (var f = PageForm.Create())
             {
-                Application.EnableVisualStyles();
                 f.ShowDialog();
             }
         }

ac8ded2515b738b3895f720fdb88ff71f9d4de96
Arvie Delgado <11804514+arviedelgado@users.noreply.github.com>
2018-02-09 16:01:57 +0800

    Roro Studio - Added Roro.Activities.Storage project to automate files and folders.

---
+++ src/Roro.Activities.Storage/FileCreateText.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Roro.Activities.Storage
+{
+    public class FileCreateText : ProcessNodeActivity
+    {
+        public Input<Text> FilePath { get; set; }
+
+        public override void Execute(ActivityContext context)
+        {
+            var filePath = context.Get(this.FilePath);
+
+            File.CreateText(filePath).Close();
+        }
+    }
+}
---
+++ src/Roro.Activities.Storage/FileExists.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Roro.Activities.Storage
+{
+    public class FileExists : DecisionNodeActivity
+    {
+        public Input<Text> FilePath { get; set; }
+
+        public override bool Execute(ActivityContext context)
+        {
+            var filePath = context.Get(this.FilePath);
+            return File.Exists(filePath);
+        }
+    }
+}

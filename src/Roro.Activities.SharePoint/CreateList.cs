c04974a80545cc49899e9fca69c95dcc6afeb6d4
Paul Luckey <paul.luckey@gimmal.com>
2018-04-30 16:47:19 -0500

    getting set up to add sharepoint bindings

---
+++ src/Roro.Activities.SharePoint/CreateList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.SharePoint.Client;
+
+namespace Roro.Activities.SharePoint
+{
+    public class CreateList : ProcessNodeActivity
+    {
+
+        public Input<Text> SiteUrl { get; set; }
+        public Input<Text> ListTitle { get; set; }
+
+        public override void Execute(ActivityContext context)
+        {
+
+            ClientContext spContext = new ClientContext(this.SiteUrl.ToString());
+            Web spWeb = spContext.Web;
+            List spList = spWeb.Lists.GetByTitle(this.ListTitle.ToString());
+            spContext.Load(spList);
+
+            try
+            {
+                spContext.ExecuteQuery();
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+
+        }
+    }
+}

f1ac9f6d91731bb9e3302ead00eaf2ce9ad648d5
Arvie Delgado <11804514+arviedelgado@users.noreply.github.com>
2018-05-01 17:05:09 -0500

    debugging arguments from UI. fixed CreateList. added CreateListItem.

---
+++ src/Roro.Activities.SharePoint/CreateListItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace Roro.Activities.SharePoint
+{
+    class CreateListItem : ProcessNodeActivity
+    {
+        public Input<Text> SiteUrl { get; set; }
+        public Input<Text> ListTitle { get; set; }
+        public Input<Text> ItemTitle { get; set; }
+
+        public override void Execute(ActivityContext context)
+        {
+            //TODO: Handle fields other than the Title field.
+            ClientContext clientContext = new ClientContext(context.Get(this.SiteUrl));
+
+            List list = clientContext.Web.Lists.GetByTitle(context.Get(this.ListTitle));
+            ListItemCreationInformation lci = new ListItemCreationInformation();
+
+            ListItem newItem = list.AddItem(lci);
+            newItem["Title"] = context.Get(this.ItemTitle);
+            newItem.Update();
+
+            clientContext.ExecuteQuery();
+
+            throw new NotImplementedException();
+        }
+    }
+}
